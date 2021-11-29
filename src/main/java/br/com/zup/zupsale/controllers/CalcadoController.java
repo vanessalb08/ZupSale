91ae71de877e515c1538777f5988185e3e167cf6
Anna Karolyne <dev.akvc@gmail.com>
2021-11-29 14:59:38 -0300

    Criar endpoint para cadastrar calcado

--- src/main/java/br/com/zup/zupsale/controllers/CalcadoController.java
+++ src/main/java/br/com/zup/zupsale/controllers/CalcadoController.java
@@ -1,10 +1,14 @@
 package br.com.zup.zupsale.controllers;
 
+import br.com.zup.zupsale.dtos.CadastroDTO;
+import br.com.zup.zupsale.models.Calcado;
 import br.com.zup.zupsale.services.CalcadoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/zupsale")
@@ -15,5 +19,12 @@ public class CalcadoController {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Calcado cadastrarCalcado(@RequestBody @Valid CadastroDTO cadastroDTO) {
+        Calcado calcado = modelMapper.map(cadastroDTO, Calcado.class);
+        return calcadoService.cadastrarCalcado(calcado);
+    }
 
 }
