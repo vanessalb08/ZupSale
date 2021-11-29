3bbfdbb267ece5bfe2d2e1c9a70f43b764eb8252
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-11-29 12:06:17 -0300

    Adicionar anotacoes da entidade calcados

--- src/main/java/br/com/zup/zupsale/models/Calcado.java
+++ src/main/java/br/com/zup/zupsale/models/Calcado.java
@@ -2,24 +2,32 @@ package br.com.zup.zupsale.models;
 
 import br.com.zup.zupsale.enuns.Categoria;
 import br.com.zup.zupsale.enuns.Genero;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Table(name = "calcados")
 public class Calcado {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Enumerated(EnumType.STRING)
     private Genero genero;
+    @Enumerated(EnumType.STRING)
     private Categoria categoria;
+    @Column(nullable = false, columnDefinition = "VARCHAR(50)")
     private String cor, marca;
+    @Column(nullable = false)
     private Integer tamanho, quantidadeDeEstoque;
+    @Column(nullable = false)
     private Double valorDaCompra;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dataDeCadastro = LocalDateTime.now();
+    @ManyToOne
+    private Estoque estoque;
 
 }
