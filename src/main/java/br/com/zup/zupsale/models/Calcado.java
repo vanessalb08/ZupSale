5e0d18e0ebbf725826b4c7914ca0d82de2838cf7
Elaine <elaine.silva@zup.com.br>
2021-11-29 09:52:19 -0300

    criar model Calcado

---
+++ src/main/java/br/com/zup/zupsale/models/Calcado.java
@@ -0,0 +1,18 @@
+package br.com.zup.zupsale.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter @NoArgsConstructor
+public class Calcado {
+    private Genero genero;
+    private Categoria categoria;
+    private String cor, marca;
+    private Integer tamanho, quantidadeDeEstoque;
+    private Double valorDaCompra;
+    private LocalDateTime dataDeCadastro = LocalDateTime.now();
+
+}
