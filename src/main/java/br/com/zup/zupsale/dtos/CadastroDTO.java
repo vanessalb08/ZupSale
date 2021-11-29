a6bed12160110a03130b81fc75c543b17c5026f3
Elaine <elaine.silva@zup.com.br>
2021-11-29 10:19:07 -0300

    criar dto Cadastro

---
+++ src/main/java/br/com/zup/zupsale/dtos/CadastroDTO.java
@@ -0,0 +1,15 @@
+package br.com.zup.zupsale.dtos;
+
+import br.com.zup.zupsale.enuns.Categoria;
+import br.com.zup.zupsale.enuns.Genero;
+import lombok.Data;
+
+@Data
+public class CadastroDTO {
+    private Genero genero;
+    private Categoria categoria;
+    private String cor, marca;
+    private Integer tamanho, quantidadeDeEstoque;
+    private Double valorDaCompra;
+
+}
