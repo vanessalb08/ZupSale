8b350e06361982b99e710692a1c45b7665f13ff8
Elaine <elaine.silva@zup.com.br>
2021-11-29 10:28:44 -0300

    Criar componente ModelMapper

---
+++ src/main/java/br/com/zup/zupsale/components/Conversor.java
@@ -0,0 +1,12 @@
+package br.com.zup.zupsale.components;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Conversor {
+    @Bean
+    public ModelMapper modelMapper() { return new ModelMapper(); }
+
+}
