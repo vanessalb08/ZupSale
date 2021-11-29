787eae0e9bca8db5f963d1162bcc050bc81fe29e
Anna Karolyne <dev.akvc@gmail.com>
2021-11-29 14:58:23 -0300

    Criar metodo para cadastrar calcado na Service

--- src/main/java/br/com/zup/zupsale/services/CalcadoService.java
+++ src/main/java/br/com/zup/zupsale/services/CalcadoService.java
@@ -1,13 +1,22 @@
 package br.com.zup.zupsale.services;
 
+import br.com.zup.zupsale.models.Calcado;
 import br.com.zup.zupsale.repositories.CalcadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CalcadoService {
 
     @Autowired
     private CalcadoRepository calcadoRepository;
+
+    public Calcado cadastrarCalcado(Calcado calcado) {
+        calcado.setDataDeCadastro(LocalDateTime.now());
+        return calcadoRepository.save(calcado);
+
+    }
 
 }
