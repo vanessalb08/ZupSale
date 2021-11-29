b2adf0a3b983c21b0e853a63e51483360518e9a6
Elaine <elaine.silva@zup.com.br>
2021-11-29 09:44:09 -0300

    first commit

---
+++ src/main/java/br/com/zup/zupsale/ZupsaleApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.zupsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupsaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/zupsale/ZupsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.zupsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupsaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
