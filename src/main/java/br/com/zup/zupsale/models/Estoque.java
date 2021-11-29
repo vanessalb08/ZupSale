e280e8b401938ad182064898f73e2d2c5d3af279
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-11-29 12:05:47 -0300

    Adicionar anotacoes da entidade

--- src/main/java/br/com/zup/zupsale/models/Estoque.java
+++ src/main/java/br/com/zup/zupsale/models/Estoque.java
@@ -1,15 +1,22 @@
 package br.com.zup.zupsale.models;
 
+import br.com.zup.zupsale.enuns.Categoria;
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Entity
+@Table(name = "estoques")
 public class Estoque {
+    @Id
+    @Column(columnDefinition = "VARCHAR(50)")
+    private String nome;
+    @OneToMany
     private List<Calcado> calcados = new ArrayList<>();
+    @Column(nullable = false)
     private Double quantidade;
 
 }
