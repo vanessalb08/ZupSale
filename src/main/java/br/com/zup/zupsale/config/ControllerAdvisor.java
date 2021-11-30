fde7502fe79e65889d31f2a6e4cfa935abb144ba
Anna Karolyne <dev.akvc@gmail.com>
2021-11-30 16:01:52 -0300

    Criar Controller Advisor com metodo de excecao calcado nao localizado

--- src/main/java/br/com/zup/zupsale/config/ControllerAdvisor.java
+++ src/main/java/br/com/zup/zupsale/config/ControllerAdvisor.java
@@ -1,2 +1,18 @@
-package br.com.zup.zupsale.config;public class ControllerAdvisor {
+package br.com.zup.zupsale.config;
+
+import br.com.zup.zupsale.exceptions.CalcadoNaoLocalizadoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvisor {
+
+    @ExceptionHandler(CalcadoNaoLocalizadoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MensagemDeErro ExcecaoDeCalcadoNaoLocalizado(CalcadoNaoLocalizadoException exception) {
+        return new MensagemDeErro(exception.getMessage());
+    }
+
 }
