00e4a4f7c18d688ffce5e44026a2f1d27bdf6805
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-11-30 14:03:48 -0300

    Inserir validacoes

--- src/main/java/br/com/zup/zupsale/dtos/CadastroDTO.java
+++ src/main/java/br/com/zup/zupsale/dtos/CadastroDTO.java
@@ -5,12 +5,18 @@ import br.com.zup.zupsale.enuns.Genero;
 import br.com.zup.zupsale.models.Estoque;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class CadastroDTO {
     private Genero genero;
     private Categoria categoria;
+    @Size(min = 3, message = "{validacao.size}")
     private String cor, marca;
+    @NotNull(message = "{validacao.not-null}")
     private Integer tamanho, quantidadeDeEstoque;
+    @NotNull(message = "{validacao.not-null}")
     private Double valorDaCompra;
 
 }
