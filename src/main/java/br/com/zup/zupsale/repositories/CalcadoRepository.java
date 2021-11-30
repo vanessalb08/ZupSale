6767836632994b6a2fd74c7bd9f509e3b9f657cc
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-11-30 11:23:37 -0300

    Criar metodo buscar por tamanho

--- src/main/java/br/com/zup/zupsale/repositories/CalcadoRepository.java
+++ src/main/java/br/com/zup/zupsale/repositories/CalcadoRepository.java
@@ -3,7 +3,10 @@ package br.com.zup.zupsale.repositories;
 import br.com.zup.zupsale.models.Calcado;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 
 public interface CalcadoRepository extends CrudRepository<Calcado, Integer> {
 
+    List<Calcado> findAllByTamanho(Integer tamanho);
 }
