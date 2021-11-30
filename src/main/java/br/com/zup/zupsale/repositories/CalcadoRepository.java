74464afa5970ccd392327b7604bab6b4a85ec1b6
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-11-30 12:05:05 -0300

    Criar metodo filtrar por valor aproximado

--- src/main/java/br/com/zup/zupsale/repositories/CalcadoRepository.java
+++ src/main/java/br/com/zup/zupsale/repositories/CalcadoRepository.java
@@ -2,6 +2,7 @@ package br.com.zup.zupsale.repositories;
 
 import br.com.zup.zupsale.enuns.Categoria;
 import br.com.zup.zupsale.models.Calcado;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -14,4 +15,7 @@ public interface CalcadoRepository extends CrudRepository<Calcado, Integer> {
     List<Calcado> findAllByMarca(String marca);
 
     List<Calcado> findAllByCategoria(Categoria categoria);
+
+    @Query(value = "SELECT * FROM calcados WHERE valor_da_compra BETWEEN :valor*0.85 AND :valor*1.15",nativeQuery = true)
+    List<Calcado> findAllByValorDaCompraBetween(double valor);
 }
