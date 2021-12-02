85daf1e72060e52dad4bdecb152a1cf7724a0681
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-12-02 10:17:38 -0300

    Criar classe de VendaSaidaDTO

--- src/main/java/br/com/zup/zupsale/dtos/VendaEntradaDTO.java
+++ src/main/java/br/com/zup/zupsale/dtos/VendaEntradaDTO.java
@@ -2,9 +2,14 @@ package br.com.zup.zupsale.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+
 @Data
-public class VendaDTO {
+public class VendaEntradaDTO {
+    @NotNull
     private Integer id, quantidade;
-    private Double valorDaVenda;
+    @DecimalMin(value = "0.01", message = "Valor tem que ser maior que zero")
+    private Double porcentagemDeLucro;
 
 }
