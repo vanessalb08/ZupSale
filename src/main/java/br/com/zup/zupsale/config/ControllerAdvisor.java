e84b59b815b6efb5dacb761ac2233cdcb82b109d
Anna Karolyne <dev.akvc@gmail.com>
2021-12-02 16:36:26 -0300

    Adicionar tratamento para excecao de enum

--- src/main/java/br/com/zup/zupsale/config/ControllerAdvisor.java
+++ src/main/java/br/com/zup/zupsale/config/ControllerAdvisor.java
@@ -4,6 +4,7 @@ import br.com.zup.zupsale.exceptions.CalcadoNaoLocalizadoException;
 import br.com.zup.zupsale.exceptions.EstoqueInsuficienteException;
 import br.com.zup.zupsale.exceptions.IdNaoEncontradoException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -48,6 +49,10 @@ public class ControllerAdvisor {
         return new MensagemDeErro(exception.getMessage());
     }
 
-
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MensagemDeErro ExcecaoDeEnumInvalido(HttpMessageNotReadableException exception) {
+        return new MensagemDeErro("Opção inválida");
+    }
 
 }
